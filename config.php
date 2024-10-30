4989bbaa38070cd45579ae582206a5f8139b0767
maurocardia <maurocardia27@gmail.com>
2024-10-30 14:02:30 -0500

    correction

--- config.php
+++ config.php
@@ -21,7 +21,7 @@ if (file_exists(__DIR__ . '/.env')) {
 function getDbConnection() {
     $host = getenv('DB_HOST');
     $user = getenv('DB_USER');
-    $password = getenv('DB_PASSWORD');
+    $password = getenv('DB_PASS');
     $dbname = getenv('DB_NAME');
     $dbport = getenv('DB_PORT');
 
