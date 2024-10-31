0ec334b7306db03411ae88f549f6316c15128513
maurocardia <maurocardia27@gmail.com>
2024-10-31 08:44:09 -0500

    correction

--- index.php
+++ index.php
@@ -12,6 +12,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Postres</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="./uploads/magdalena.png" type="image/x-icon">
     <style>
         /* Estilos para el modal */
         .modal {
